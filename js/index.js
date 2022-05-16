df6e235da72219ce987a6f34530e00304397b5f7
danielmweb <ldanielmoreno-2014@outlook.com>
2022-05-16 10:23:31 -0300

    added automatic translation for browser languages other than portuguese (defaults to english if the language is not portuguese)

--- js/index.js
+++ js/index.js
@@ -4,9 +4,14 @@ function googleTranslateElementInit() {
 		{ pageLanguage: "pt" },
 		"google_translate_element"
 	);
-	navigator.language = "";
 }
 
 window.addEventListener("load", () => {
+	//if the user language is not portuguese, default it to english
+	if (navigator.language.split("-")[0] != "pt") {
+		document.cookie = "googtrans=/pt/en;expires=session";
+	} else {
+		document.cookie = "googtrans=/pt/pt;expires=session";
+	}
 	googleTranslateElementInit();
 });
